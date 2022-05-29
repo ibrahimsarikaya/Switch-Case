19b9cdc939754e9d6ad2bc60309062f2e009775f
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-30 01:59:37 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Switch_Case // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız.");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız.");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs Ayındasınız.");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                break;
+            }
+            switch (month)
+            {
+                case 3:
+                case 4:
+                case 5:
+                 Console.WriteLine("İlkbahardasınız");
+                 break;
+                default:
+                break;
+            }
         }
     }
 }
